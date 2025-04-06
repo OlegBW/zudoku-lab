79089c699d32cea96f8515c385d03e6949fb1e14
OlegBW <olegkorostilov58@gmail.com>
2025-04-06 22:48:56 +0300

    Updates

--- assets/installation-Qb23rytn.js
+++
@@ -1,8 +0,0 @@
-import{X as o,j as e}from"./entry.client-CZ1tIu3b.js";const i=[{depth:3,value:"1. Clone the repository",id:"1-clone-the-repository"},{depth:3,value:"2. Install dependencies",id:"2-install-dependencies"}],a={title:"Installation"};function s(t){const n={code:"code",h3:"h3",pre:"pre",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h3,{id:"1-clone-the-repository",children:"1. Clone the repository"}),`
-`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`git clone https://github.com/OlegBW/openapi-lab.git
-cd openapi-lab
-`})}),`
-`,e.jsx(n.h3,{id:"2-install-dependencies",children:"2. Install dependencies"}),`
-`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`pip install -r requirements.txt
-`})})]})}function r(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{r as default,a as frontmatter,i as tableOfContents};
-//# sourceMappingURL=installation-Qb23rytn.js.map
